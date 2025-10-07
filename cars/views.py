271e90d612d1bf10ede23657a310f0b6ac1cacad
gabriel <gabrielseg.ti@gmail.com>
2025-10-07 17:37:09 -0300

    criação html cars e base e views

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('brand')
     search = request.GET.get('search')
 
     if search:
