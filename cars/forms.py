b93d8cb124b9053f51cced71c994fa2a38ed6d23
gabriel <gabrielseg.ti@gmail.com>
2025-10-10 15:18:05 -0300

    criacao pagina cadastrar carros, forms e validacao

---
+++ cars/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from cars.models import Car
+
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value is not None and value < 20000:
+            self.add_error('value', 'O valor do carro não pode ser menor que R$20.000')
+        return value
