e0d56a22eb6021ecfce431046177f12b8bebd9a7
gabriel <gabrielseg.ti@gmail.com>
2025-10-16 09:15:37 -0300

    tela de login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 def register_view(request):
     if request.method == 'POST':
@@ -10,3 +11,24 @@ def register_view(request):
     else:
         user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')
+        else:
+            login_form = AuthenticationForm()
+            return render(request, 'login.html', {'login_form': login_form, 'error': 'Invalid credentials'})
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
