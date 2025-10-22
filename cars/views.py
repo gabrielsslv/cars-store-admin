6955475eea37f89a1a8c89c822ed6c17aa1b11bc
gabriel <gabrielseg.ti@gmail.com>
2025-10-22 14:31:20 -0300

    update e delete carro

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import redirect, render
-from django.shortcuts import render
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
     
 class CarsListView(ListView):
     model = Car
@@ -23,8 +21,20 @@ class NewCarCreateView(CreateView):
     template_name = 'new_car.html'
     success_url = '/cars/'
 
-
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
     context_object_name = 'car'
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
